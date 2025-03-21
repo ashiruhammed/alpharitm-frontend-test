b854fd8d519f1abba28e7a420bb860c261622caf
ashiruhammed <hammedashiru89@gmail.com>
2025-03-21 22:43:13 +0100

    Add LogoIcon component to icons library

---
+++ components/icons/logo.tsx
@@ -0,0 +1,140 @@
+import * as React from 'react';
+const LogoIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg width={48} height={48} viewBox='0 0 48 48' fill='none' {...props}>
+    <g clipPath='url(#clip0_80482_844)'>
+      <rect width={48} height={48} rx={7.38462} fill='white' />
+      <rect
+        width={48}
+        height={48}
+        rx={7.38462}
+        fill='url(#paint0_linear_80482_844)'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M23.9999 3.0584C12.4341 3.0584 3.05816 12.4343 3.05816 24.0001C3.05816 35.566 12.4341 44.9419 23.9999 44.9419C35.5657 44.9419 44.9417 35.566 44.9417 24.0001C44.9417 12.4343 35.5657 3.0584 23.9999 3.0584ZM2.94165 24.0001C2.94165 12.37 12.3698 2.94189 23.9999 2.94189C35.6301 2.94189 45.0582 12.37 45.0582 24.0001C45.0582 35.6303 35.6301 45.0584 23.9999 45.0584C12.3698 45.0584 2.94165 35.6303 2.94165 24.0001Z'
+        fill='#D0D5DD'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M24 18.117C20.7506 18.117 18.1165 20.7511 18.1165 24.0005C18.1165 27.2499 20.7506 29.884 24 29.884C27.2494 29.884 29.8835 27.2499 29.8835 24.0005C29.8835 20.7511 27.2494 18.117 24 18.117ZM18 24.0005C18 20.6868 20.6863 18.0005 24 18.0005C27.3137 18.0005 30 20.6868 30 24.0005C30 27.3142 27.3137 30.0005 24 30.0005C20.6863 30.0005 18 27.3142 18 24.0005Z'
+        fill='#D0D5DD'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M23.9999 19.9803C21.7801 19.9803 19.9805 21.7798 19.9805 23.9997C19.9805 26.2196 21.7801 28.0191 23.9999 28.0191C26.2198 28.0191 28.0194 26.2196 28.0194 23.9997C28.0194 21.7798 26.2198 19.9803 23.9999 19.9803ZM19.864 23.9997C19.864 21.7155 21.7157 19.8638 23.9999 19.8638C26.2841 19.8638 28.1359 21.7155 28.1359 23.9997C28.1359 26.2839 26.2841 28.1356 23.9999 28.1356C21.7157 28.1356 19.864 26.2839 19.864 23.9997Z'
+        fill='#D0D5DD'
+      />
+      <path d='M23.9417 0H24.0582V48H23.9417V0Z' fill='#D0D5DD' />
+      <path
+        d='M48 23.9414L48 24.0579L-5.09259e-09 24.0579L0 23.9414L48 23.9414Z'
+        fill='#D0D5DD'
+      />
+      <path d='M39.9028 0H40.0193V48H39.9028V0Z' fill='#D0D5DD' />
+      <path d='M15.9612 0H16.0777V48H15.9612V0Z' fill='#D0D5DD' />
+      <path d='M31.9224 0H32.0389V48H31.9224V0Z' fill='#D0D5DD' />
+      <path d='M7.98047 0H8.09697V48H7.98047V0Z' fill='#D0D5DD' />
+      <path
+        d='M48 39.9023L48 40.0188L-5.09259e-09 40.0188L0 39.9023L48 39.9023Z'
+        fill='#D0D5DD'
+      />
+      <path
+        d='M48 15.9609L48 16.0774L-5.09259e-09 16.0774L0 15.9609L48 15.9609Z'
+        fill='#D0D5DD'
+      />
+      <path
+        d='M48 31.9219L48 32.0384L-5.09259e-09 32.0384L0 31.9219L48 31.9219Z'
+        fill='#D0D5DD'
+      />
+      <path
+        d='M48 7.98047L48 8.09697L-5.09259e-09 8.09697L0 7.98047L48 7.98047Z'
+        fill='#D0D5DD'
+      />
+      <g filter='url(#filter0_dd_80482_844)'>
+        <circle cx={24} cy={24} r={12} fill='#1774FD' />
+      </g>
+      <path
+        d='M0 24H48C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24Z'
+        fill='white'
+        fillOpacity={0.2}
+      />
+    </g>
+    <rect
+      x={0.830769}
+      y={0.830769}
+      width={46.3385}
+      height={46.3385}
+      rx={6.55385}
+      stroke='#D0D5DD'
+      strokeWidth={1.66154}
+    />
+    <defs>
+      <filter
+        id='filter0_dd_80482_844'
+        x={-4.61539}
+        y={0.923077}
+        width={57.2308}
+        height={57.2308}
+        filterUnits='userSpaceOnUse'
+        colorInterpolationFilters='sRGB'>
+        <feFlood floodOpacity={0} result='BackgroundImageFix' />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          result='hardAlpha'
+        />
+        <feOffset dy={5.53846} />
+        <feGaussianBlur stdDeviation={5.53846} />
+        <feColorMatrix
+          type='matrix'
+          values='0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.06 0'
+        />
+        <feBlend
+          mode='normal'
+          in2='BackgroundImageFix'
+          result='effect1_dropShadow_80482_844'
+        />
+        <feColorMatrix
+          in='SourceAlpha'
+          type='matrix'
+          values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0'
+          result='hardAlpha'
+        />
+        <feOffset dy={5.53846} />
+        <feGaussianBlur stdDeviation={8.30769} />
+        <feColorMatrix
+          type='matrix'
+          values='0 0 0 0 0.0627451 0 0 0 0 0.0941176 0 0 0 0 0.156863 0 0 0 0.1 0'
+        />
+        <feBlend
+          mode='normal'
+          in2='effect1_dropShadow_80482_844'
+          result='effect2_dropShadow_80482_844'
+        />
+        <feBlend
+          mode='normal'
+          in='SourceGraphic'
+          in2='effect2_dropShadow_80482_844'
+          result='shape'
+        />
+      </filter>
+      <linearGradient
+        id='paint0_linear_80482_844'
+        x1={24}
+        y1={0}
+        x2={24}
+        y2={48}
+        gradientUnits='userSpaceOnUse'>
+        <stop stopColor='white' />
+        <stop offset={1} stopColor='#D0D5DD' />
+      </linearGradient>
+      <clipPath id='clip0_80482_844'>
+        <rect width={48} height={48} rx={7.38462} fill='white' />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default LogoIcon;
